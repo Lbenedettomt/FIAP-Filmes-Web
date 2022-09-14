124912e3f1ace28de7eef1179f855416967e9d01
Luigi Di Benedetto <47431183+Lbenedettomt@users.noreply.github.com>
2022-09-14 00:25:37 -0300

    First Commit

---
+++ dados.js
@@ -0,0 +1,16 @@
+//console.log("carregando...")
+const dados =
+[
+    {
+        poster: "https://www.themoviedb.org/t/p/w220_and_h330_face/5qMupAz4A9EN5Rutrkmov45mNXZ.jpg",
+        titulo: "Cobra Kay",
+        nota: "8.9",
+        detalhes: "2"
+    },
+    {
+        poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zduyzcjyahZtP4fGZE6789h04IM.jpg",
+        titulo: "Rogai por nós",
+        nota: "6.7",
+        detalhes: "./rogaiDetails.html"
+    }
+]
---
+++ dadosSeries.js
@@ -0,0 +1,10 @@
+//console.log("carregando...")
+const dados =
+[
+    {
+        poster: "https://www.themoviedb.org/t/p/w220_and_h330_face/r2w5UNf2mO2Mdl4q6HopuBms6XM.jpg",
+        titulo: "Stranger Things",
+        nota: "8.6",
+        detalhes: "./strangerDetails.html"
+    }
+]
